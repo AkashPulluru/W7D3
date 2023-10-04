28a23d9c49d970d218d8dbfdb8e76faed6a2fb24
Akash Pulluru <akashpulluru@gmail.com>
2023-10-04 14:15:01 -0400

    initial user buildout

---
+++ practice-for-ch-rails-goal-app-long-practice-main/practice-for-ch-rails-goal-app-long-practice-main/goal-app/app/models/user.rb
@@ -0,0 +1,55 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+    before_validation :ensure_session_token
+
+    validates :username, :session_token, presence: true, uniqueness: true 
+    validates :password_digest, presence: true
+    validates :password, length: { minimum: 6, allow_nil: true }
+
+    def generate_session_token
+        SecureRandom::urlsafe_base64
+    end 
+
+    def reset_session_token
+        self.session_token = self.generate_session_token
+        self.save! 
+        self.session_token
+    end 
+
+    def ensure_session_token
+        self.session_token ||= self.generate_session_token
+    end 
+
+    attr_reader :password 
+
+    def password=(password)
+        @password = password 
+        self.password_digest = BCrypt::Password.create(password)
+    end 
+
+    def is_password?(password)
+        password_object = BCrypt::Password.new(self.password_digest)
+        password_object.is_password?(password)
+    end 
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+
+        if user && user.is_password?(password)
+            user 
+        else 
+            nil 
+        end 
+    end 
+
+end 
